40cd504f02f81812e3bb26e00aed2f7c1c9b014b
mani-353 <nandikollamanikanta586@gmail.com>
2024-06-28 13:33:48 +0530

    adding proposal.sol file

--- contracts/proposal.sol
+++ contracts/proposal.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 contract ProposalContract {
-
+    uint256 private counter;
     struct Proposal {
         string title;//add the title of the proposal
         string description; // Description of the proposal
@@ -15,4 +15,8 @@ contract ProposalContract {
     }
 
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+    function create(string calldata _title,string calldata _description, uint256 _total_vote_to_end) external {
+        counter += 1;
+        proposal_history[counter] = Proposal(_title,_description, 0, 0, 0, _total_vote_to_end, false, true);
+}
 }
